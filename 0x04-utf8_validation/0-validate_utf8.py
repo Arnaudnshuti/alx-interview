c1b767b42264db4f10967316073b4031d7709450
Arnaudnshuti <arnaudnshutidjadja@gmail.com>
2024-07-04 15:20:28 -0700

    alx-interview

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,11 +1,40 @@
 #!/usr/bin/python3
-"""
-UTF8 validation module
-"""
+""" UTF-8 Validation """
+
 
 def validUTF8(data):
     """
-    Validates if data consists of
-    valid utf-8 encoded characters
+    Method that determines if a given data set represents a valid
+    UTF-8 encoding.
     """
-    pass
+    number_bytes = 0
+
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
+
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                    return False
+
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
